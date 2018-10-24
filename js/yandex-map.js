2cea72599416dbfaea2f5af41cbc4bcdd497c61d
Ligalaiz <slavakuz88@gmail.com>
2018-10-24 16:33:54 +0300

    Правки

--- js/yandex-map.js
+++ js/yandex-map.js
@@ -27,3 +27,36 @@ function init () {
   myMap.geoObjects.add(myPlacemark);
 }
 })();
+
+
+
+
+$(document).ready(function () {
+  var contacts = document.querySelector(".contacts");
+  if (window.innerWidth < 530) {
+      contacts.removeAttribute('id');
+  }
+});
+
+// 
+// $(window).addEventListener("resize", function() {
+//   var contacts = document.querySelector(".contacts");
+//   if (window.innerWidth < 530) {
+//       contacts.getAttribute('id');
+//     }
+// }, false);
+
+
+// $(window).resize(function() {
+// var contacts = document.querySelector(".contacts");
+// if (document.documentElement.clientWidth > 530) {
+//     contacts.getAttribute('id');
+//   }
+// });
+//
+// $(window).resize(function() {
+// var contacts = document.querySelector(".contacts");
+// if (document.documentElement.clientWidth < 530) {
+//     contacts.removeAttribute('id');
+// }
+// });
